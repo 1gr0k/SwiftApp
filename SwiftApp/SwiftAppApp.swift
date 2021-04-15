cc6d2595427c3de4a0c30e8fed601e193cab0899
1gr0k <43414453+1gr0k@users.noreply.github.com>
2021-04-15 12:51:12 +0400

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Андрей Калямин on 15.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
